9c2f2761e2f8ac35cd6fccb075323cd5b9321d4a
Anas Ansari <anasansari056789@gmail.com>
2025-04-02 12:08:08 +0530

    Solved Problem: Maximum Value of an Ordered Triplet I

---
+++ Array/Maximum Value of an Ordered Triplet I.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+  long long maximumTripletValue(vector<int> &nums)
+  {
+    int n = nums.size();
+    long long ans = 0;
+
+    for (int i = 0; i < n - 2; i++)
+    {
+      for (int j = i + 1; j < n - 1; j++)
+      {
+        for (int k = j + 1; k < n; k++)
+        {
+          long long val = (1LL * (nums[i] - nums[j])) * nums[k];
+          ans = max(ans, val);
+        }
+      }
+    }
+
+    return ans;
+  }
+};
